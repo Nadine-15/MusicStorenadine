566125e1f93d528bad4beb4ea2c5ec8ea89a64f8
Nadine Paquette <200395742@student.georgianc.on.ca>
2019-10-07 17:22:01 -0400

    Added migration thing today

---
+++ MusicStorenadine/Migrations/20190930222348_Migration1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicStorenadine.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    GenreID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.GenreID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Song",
+                columns: table => new
+                {
+                    SongID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AlbumID = table.Column<int>(nullable: false),
+                    SongName = table.Column<string>(nullable: true),
+                    Artist = table.Column<string>(nullable: true),
+                    Featuring = table.Column<string>(nullable: true),
+                    RunTimeSeconds = table.Column<int>(nullable: false),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    SongID1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Song", x => x.SongID);
+                    table.ForeignKey(
+                        name: "FK_Song_Song_SongID1",
+                        column: x => x.SongID1,
+                        principalTable: "Song",
+                        principalColumn: "SongID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Album",
+                columns: table => new
+                {
+                    AlbumID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GenreID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Artist = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Album", x => x.AlbumID);
+                    table.ForeignKey(
+                        name: "FK_Album_Genre_GenreID",
+                        column: x => x.GenreID,
+                        principalTable: "Genre",
+                        principalColumn: "GenreID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Album_GenreID",
+                table: "Album",
+                column: "GenreID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Song_SongID1",
+                table: "Song",
+                column: "SongID1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Album");
+
+            migrationBuilder.DropTable(
+                name: "Song");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
---
+++ MusicStorenadine/Migrations/20191007211324_MigrationYeet.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicStorenadine.Migrations
+{
+    public partial class MigrationYeet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
